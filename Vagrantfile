0fcda1973cd92ba08a6231c2a6fa36059e5d0aaa
Mark Maglana <mmaglana@gmail.com>
2013-12-19 19:59:51 -0800

    Give the node a friendly name

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,6 @@
 Vagrant.configure("2") do |config|
 
-  config.vm.define :server do |role|
+  config.vm.define :node1 do |role|
     role.vm.network   :private_network, ip: "192.168.251.100"
     role.vm.box      = "ceph_base"
     role.vm.hostname = "server"
--- setup/client.sh
+++ setup/client.sh
@@ -4,11 +4,12 @@ su vagrant -c "ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa"
 
 sudo apt-get install -y sshpass
 
-echo "Host 192.168.251.100
+echo "Host node1
+    HostName 192.168.251.100
     StrictHostKeyChecking no
     User ceph" > /home/vagrant/.ssh/config
 
-su vagrant -c "sshpass -p '123qwe' ssh-copy-id ceph@192.168.251.100"
+su vagrant -c "sshpass -p '123qwe' ssh-copy-id ceph@node1"
 
 wget -q -O- 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc' | sudo apt-key add -
 echo deb http://ceph.com/debian-emperor/ $(lsb_release -sc) main | sudo tee /etc/apt/sources.list.d/ceph.list
@@ -16,4 +17,4 @@ sudo apt-get update
 sudo apt-get install ceph-common -y
 sudo apt-get install -y ceph-deploy
 
-su vagrant -c "ceph-deploy install 192.168.251.100"
+su vagrant -c "ceph-deploy install node1"
